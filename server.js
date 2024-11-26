0d9c98218bf1e019eecc008cd9e871ee8a951c01
Ochiponu-Awodi <awodiomale@gmail.com>
2024-11-26 11:02:53 +0100

    Working With JSON

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ const path = require('path');
 const app = express();
 
 // setup static folder
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+let posts = [{
+    id: 1,
+    title: 'Post 1',
+},
+{
+    id: 2,
+    title: 'Post 2',
+},
+{
+    id: 3,
+    title: 'Post 3',
+}
+]
+
+app.get('/api/posts/', (req, res) => {
+    res.json(posts);
+})
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
