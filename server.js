8f6b8c7048ab1c2977da132ee83e65cee983ff5d
Ochiponu-Awodi <awodiomale@gmail.com>
2024-11-28 09:12:17 +0100

    Catch all error middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import path from 'path';
 import posts from './routes/posts.js'
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -21,6 +22,7 @@ app.use(logger);
 app.use('/api/posts', posts);
 
 // Error middleware
+app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
